2f9fc72a16d917f44264e90579a968d09debbe9d
s1243001 <s1243001@gm.ncue.edu.tw>
2025-05-14 20:12:51 +0800

    app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ vis_params_001 = {'min': 0, 'max': 4, 'palette': palette}
 
 my_Map.centerObject(result001, 8)
 my_Map.addLayer(result001, vis_params_001, 'Labelled clusters')
-
+my_Map.add_legend(title='Land Cover Type', legend_dict = legend_dict, position = 'bottomright')
 
 clusterer_XMeans = ee.Clusterer.wekaXMeans().train(training001)
 
@@ -102,7 +102,6 @@ vis_params_002 = {'min': 0, 'max': 7, 'palette': palette}
 my_Map = geemap.Map()
 my_Map.centerObject(result002, 8)
 my_Map.addLayer(result002, vis_params_002, 'Labelled clusters')
-my_Map.add_legend(title='Land Cover Type', legend_dict2 = legend_dict2, position = 'bottomright')
 
 
 my_Map = geemap.Map()
