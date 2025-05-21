4b90c3581941763daf067ae5d76731f7dad04ef0
s1243001 <s1243001@gm.ncue.edu.tw>
2025-05-21 17:01:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 from datetime import date
 
-st.set_page_config(layout="wide", page_title="這是Streamlit App第二次練習！")
-
-st.title("應用程式主頁")
+st.set_page_config(layout="wide", page_title="自行車環島Go！Go！")
 
 st.markdown(
     """
