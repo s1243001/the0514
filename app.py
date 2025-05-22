a63a1e27abbab661131668617576c9fd59324084
s1243001 <s1243001@gm.ncue.edu.tw>
2025-05-22 16:48:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ style = {
     "fillColor": "none" # No fill color
 }
 route1_url = "https://github.com/s1243001/the0514/blob/main/route1.geojson"
-m.add_basemap(basemap)
 m.add_geojson(route1_url, layer_name="route1", style=style)
 m.to_streamlit(height=700)
 
