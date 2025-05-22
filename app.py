66657cd076f2d59e64e4a34616da006e9362abe7
s1243001 <s1243001@gm.ncue.edu.tw>
2025-05-22 16:59:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,10 @@ route1 = "route1.geojson"
 m.add_geojson(route1, layer_name="route1",style=style)
 m.to_streamlit(height=700)
 
+option = st.selectbox(
+    '選擇你的路線',
+    ['1-1', '1-2', '1-3', '1-4','1-5','1-6','1-7','1-8','1-9','1-10,'1-11','1-12','1-13','1-14','1-15','1-16','1-17','1-18','1-19','1-20','1-21','1-22','1-23','1-24','1-25'])
+st.text(f'你的答案：{option}')
 
 with st.expander("展示gif檔"):
     st.image("2sfca1500m.jpg")
