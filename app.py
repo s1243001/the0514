476dffe06004f93f11750f497f8b1700fbc2fefa
s1243001 <s1243001@gm.ncue.edu.tw>
2025-05-22 16:24:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,31 +25,7 @@ st.markdown(markdown)
 
 
 
-st.title("選擇日期區間")
-
-
-# 初始化 session_state
-#if 'start_date' not in st.session_state:
-#    st.session_state['start_date'] = date(2024, 1, 1)
-#if 'end_date' not in st.session_state:
-#    st.session_state['end_date'] = date.today()
-
-st.session_state['start_date'] = date(2024, 1, 1)
-st.session_state['end_date'] = date.today()
-
-
-# 日期選擇器
-start_date = st.date_input(label = "選擇起始日期", value = st.session_state['start_date'], min_value = date(2018, 1, 1), max_value = date.today())
-end_date = st.date_input(label = "選擇結束日期", value = st.session_state['end_date'], min_value = start_date, max_value = date.today())
-
-# 儲存使用者選擇
-st.session_state['start_date'] = start_date
-st.session_state['end_date'] = end_date
-
-st.success(f"目前選擇的日期區間為：{start_date} 到 {end_date}")
-
-
-st.title("利用擴充器示範")
+m = leafmap.Map(center = [42.5, -4.0], zoom = 7 , minimap_control=True)
 
 with st.expander("展示gif檔"):
     st.image("2sfca1500m.jpg")
