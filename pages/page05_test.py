c85f78fc9c13a008a7772b92ad3076c6a51a1166
s1243001 <s1243001@gm.ncue.edu.tw>
2025-06-04 16:44:44 +0800

    Create page05_test.py

---
+++ pages/page05_test.py
@@ -0,0 +1,31 @@
+
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+st.set_page_config(layout="wide")
+
+markdown = """
+National Changhua University of Education
+<https://www.ncue.edu.tw/>
+"""
+
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
+
+st.title("資料來源")
+
+with st.expander("See source code"):
+    with st.echo():
+        filepath = "us_cities.csv"
+        m = leafmap.Map(center=[40, -100], zoom=4)
+        m.add_heatmap(
+            filepath,
+            latitude="latitude",
+            longitude="longitude",
+            value="pop_max",
+            name="Heat map",
+            radius=20,
+        )
+m.to_streamlit(height=700)
