62c664369522a367b4c707a90097fd89df5d573d
s1243001 <s1243001@gm.ncue.edu.tw>
2025-06-07 11:49:29 +0800

    Update page05_test.py

--- pages/page05_test.py
+++ pages/page05_test.py
@@ -18,14 +18,14 @@ st.title("🫂關於我們的網站")
 
 
 markdown = """
-感謝您使用朝聖者之路平台！<br>  
-由於期末時間緊迫，目前尚未實現使用者自行標示地點與整合紀錄的功能<br>  
-我們誠摯邀請使用平台的朋友，將沿途探索到的美食店家與景點資訊分享給我們<br>  
-您的寶貴建議將有助於我們在後續更新中，繼續優化平台內容、服務更多朝聖者<br>  
+感謝您使用台灣自行車環島一號線旅遊網！<br>  
+抱歉由於期末時間緊迫，目前尚未實現更多語言選項及其他功能<br>  
+如果您在環島的旅途中，發現令人印象深刻的景色或是山珍海味，歡迎將美食店家與景點資訊分享給我們<br>  
+您的建議有助於我們在日後更新中提供更好的使用者體驗<br>  
 
 ---
 
-**期待在朝聖者之路與您相遇！**
+**享受台灣自行車旅行的美好，祝你有個難忘的旅程！**
 
 ---
 """
@@ -33,19 +33,19 @@ st.markdown(markdown, unsafe_allow_html=True)
 
 st.title("☎️ 聯絡資訊")
 markdown2 = """📧 **Email**  
-- Chinchilla: [zhongchinchilla0529@gmail.com](mailto:zhongchinchilla0529@gmail.com)  
-- Magi: [magi23570902@gmail.com](mailto:magi23570902@gmail.com)  
-- Rich: [s1143012@gm.ncue.edu.tw](mailto:s1143012@gm.ncue.edu.tw)  
+- Wu: [apord67@gmail.com](mailto:apord67@gmail.com)  
+- Wang: [wang940125@gmail.com](mailto:wang940125@gmail.com)  
 
 
 🌍 **Resources**  
-- Streamlit Map Template: [GitHub Repository](https://github.com/opengeos/streamlit-map-template)
-- Camino Official Website: [website](https://www.caminodesantiago.gal/en/inicio)
-- Food Recommendation: [Spain](https://www.marieclaire.com.tw/lifestyle/taste/72322)、[Portugal](https://hsuanslife.com/portugal-food/)
+- Streamlit Map Template: [GitHub Repository](https://github.com/s1243001/the0514/tree/main)
+- 台灣騎跡全國自行車單一總入口網: [website](https://taiwanbike.tw/bikeRoute/search)
+- 交通部觀光署環島旅遊指南: [website](https://www.taiwan.net.tw/att/files/%E8%87%AA%E8%A1%8C%E8%BB%8A%E7%92%B0%E5%B3%B6%E6%8C%87%E5%8D%97.pdf)
+- Attraction & Food Recommendation: (景點、食物參考網站以至於介紹後方)
 
 ---
 
-<span style="color: grey;">最近更新日期: 2024/12/30 晚上10:00</span>
+<span style="color: grey;">最近更新日期: 2025/06/07 上午12:00</span>
 
 
 
